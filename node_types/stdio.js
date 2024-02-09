85b0129d6cba9926bbf5a95901633c35376e18f6
Chai <7232280+Chailotl@users.noreply.github.com>
2024-02-09 14:00:46 -0500

    Added ansi outputs to stdio node

--- node_types/stdio.js
+++ node_types/stdio.js
@@ -5,12 +5,14 @@ const capcon = require('capture-console');
 module.exports = class StdioNode extends Node
 {
 	getDescription() { return 'This node will capture stdout and sterr and emit them as string events.'; }
-	getOutputs() { return ['stdout', 'stderr']; }
+	getOutputs() { return ['stdout', 'stderr', 'stdout_ansi', 'stderr_ansi']; }
 
 	onActivate()
 	{
 		capcon.startCapture(process.stdout, stdout =>
 		{
+			this.sendEvent('stdout_ansi', new Event(stdout));
+
 			stdout = this.filterAnsiEscapeSeq(stdout);
 
 			this.sendEvent('stdout', new Event(stdout));
@@ -18,6 +20,8 @@ module.exports = class StdioNode extends Node
 
 		capcon.startCapture(process.stderr, stderr =>
 		{
+			this.sendEvent('stderr_ansi', new Event(stderr));
+
 			stderr = this.filterAnsiEscapeSeq(stderr);
 
 			this.sendEvent('stderr', new Event(stderr));
